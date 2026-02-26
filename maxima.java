4c9dc98893c582c8f7d15f1851fe2966143278fb
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2026-02-26 17:50:19 +0600

    4

--- maxima.java
+++ maxima.java
@@ -1,25 +1,25 @@
 import java.util.Scanner;
-public class maxima 
-{
-    public static void main(String[] args)
-    {
-        Scanner sc= new Scanner(System.in);
-        int[] number=new int[5];
-        for(int i=0;i<5;i++)
-        {
-         number[i]=sc.nextInt();
 
+public class maxima {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int[] number = new int[5];
+
+        for(int i = 0; i < 5; i++) {
+            number[i] = sc.nextInt();
         }
-      int max =number[0];
-      for(int i=0;i<5;i++)
-      {
-        if(max<number[i])
-        {
-            max=number[i];
+
+        int max = number[0];
+
+        for(int i = 1; i < 5; i++) {
+            if(max < number[i]) {
+                max = number[i];
+            }
         }
-         System.out.println(max);
-      }
-    
+
+        System.out.println("Max = " + max);
+        sc.close();
     }
 }
 //input: 5 10 15 20 25
